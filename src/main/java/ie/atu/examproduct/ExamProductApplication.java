7d1c7b9ec9f1cd843609d0689ecaa1fa3d9c7ba5
Denis <G00403126@atu.ie>
2023-11-07 16:14:18 +0000

    .

---
+++ src/main/java/ie/atu/examproduct/ExamProductApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.examproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamProductApplication.class, args);
+    }
+
+}
